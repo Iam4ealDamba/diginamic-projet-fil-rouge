8712c7e4fa9f4276f72a09e14743b6bfccd58722
lysiandon <don.lysiane@gmail.com>
2024-07-16 15:05:05 +0200

    creating repository mission

---
+++ backend/src/main/java/fr/projet/diginamic/backend/repositories/MissionRepository.java
@@ -0,0 +1,12 @@
+package fr.projet.diginamic.backend.repositories;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * Repository interface for {@link Mission} entities, providing methods to perform
+ * operations on the database.
+ */
+@Repository
+public interface MissionRepository {
+
+}
