734bab42eb5dc0e465f02959175300431ec1d12d
Iam4ealDamba <prayforsouls720@gmail.com>
2024-07-16 10:09:40 +0200

    Initialisation du projet

---
+++ backend/src/test/java/fr/projet/diginamic/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.projet.diginamic.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
