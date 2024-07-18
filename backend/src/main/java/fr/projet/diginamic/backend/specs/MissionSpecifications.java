b88f2fdd4dba77bfc1a66eb9d822b900f37f43ea
lysiandon <don.lysiane@gmail.com>
2024-07-18 13:53:09 +0200

    added javadoc

--- backend/src/main/java/fr/projet/diginamic/backend/specs/MissionSpecifications.java
+++ backend/src/main/java/fr/projet/diginamic/backend/specs/MissionSpecifications.java
@@ -73,6 +73,16 @@ public class MissionSpecifications {
         };
     }
 
+    /**
+     * Creates a specification to filter missions based on their label.
+     * This method supports partial matching, meaning that any mission containing
+     * the specified label substring in a case-insensitive manner will be returned.
+     *
+     * @param label The label or part of the label of the mission to filter by.
+     *              If null, no filtering will be applied based on the label.
+     * @return A Specification<Mission> that can be used to filter queries by the
+     *         mission label.
+     */
     public static Specification<Mission> hasLabel(String label) {
         return (root, query, criteriaBuilder) -> {
             if (label == null) {
@@ -115,6 +125,23 @@ public class MissionSpecifications {
         return spec;
     }
 
+    /**
+     * Builds a composite specification for filtering missions based on status,
+     * nature, and label.
+     * This method is intended for use by employees to refine their mission queries,
+     * allowing them to apply filters for status, nature of the mission, and label.
+     * Each parameter is optional; if provided, it is used to filter the missions;
+     * if not provided or empty, that criterion is not used in the filter.
+     *
+     * @param status The desired status of missions to filter by. If null or empty,
+     *               no status filter is applied.
+     * @param nature The nature of the missions to filter by. If null or empty, no
+     *               nature filter is applied.
+     * @param label  The label of the missions to filter by. If null or empty, no
+     *               label filter is applied.
+     * @return A Specification<Mission> that can be used to filter missions based on
+     *         the provided criteria.
+     */
     public static Specification<Mission> filterMissionsByCriteriaForEmployee(String status, String nature,
             String label) {
         Specification<Mission> spec = Specification.where(null);
