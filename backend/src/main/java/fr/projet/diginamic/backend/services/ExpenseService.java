8a48b84ad63ed253f23f2bfda0903ad45ad42965
Thiblt <t.pouplard@gmx.fr>
2024-07-18 09:58:07 +0200

    bug fix

--- backend/src/main/java/fr/projet/diginamic/backend/services/ExpenseService.java
+++ backend/src/main/java/fr/projet/diginamic/backend/services/ExpenseService.java
@@ -67,24 +67,24 @@ public class ExpenseService {
 		return expenseSave;
 	}
 	
-	public void exportExpense(Long id, HttpServletResponse response) throws IOException, DocumentException{
-		Expense expense= expenseRepo.findById(id).orElse(null);
-		
-		response.setHeader("Content-Disposition", "attachment; filename=\"NoteDeFrais.pdf\"");
-		Document document = new Document(PageSize.A4);
-		PdfWriter.getInstance(document, response.getOutputStream());
-		document.open();
-		document.addTitle("Fiche");
-		document.newPage();
-		BaseFont baseFont = BaseFont.createFont(BaseFont.HELVETICA, BaseFont.WINANSI, BaseFont.EMBEDDED);
-		Phrase p1 = new Phrase("Nom departement: "+dGov.getNom()+", code departement:"+dGov.getCode() , new Font(baseFont, 32.0f, 1, new BaseColor(0, 51, 80)));
-		document.add(p1);
-		
-		for(Ville ville: villes) {
-			Phrase p = new Phrase("Nom ville: "+ville.getNom()+", nombre d'habitants:"+ville.getNbHabitant() , new Font(baseFont, 32.0f, 1, new BaseColor(0, 51, 80)));
-			document.add(p);
-		}
-		document.close();
-		response.flushBuffer();
-	}
+//	public void exportExpense(Long id, HttpServletResponse response) throws IOException, DocumentException{
+//		Expense expense= expenseRepo.findById(id).orElse(null);
+//		
+//		response.setHeader("Content-Disposition", "attachment; filename=\"NoteDeFrais.pdf\"");
+//		Document document = new Document(PageSize.A4);
+//		PdfWriter.getInstance(document, response.getOutputStream());
+//		document.open();
+//		document.addTitle("Fiche");
+//		document.newPage();
+//		BaseFont baseFont = BaseFont.createFont(BaseFont.HELVETICA, BaseFont.WINANSI, BaseFont.EMBEDDED);
+//		Phrase p1 = new Phrase("Nom departement: "+dGov.getNom()+", code departement:"+dGov.getCode() , new Font(baseFont, 32.0f, 1, new BaseColor(0, 51, 80)));
+//		document.add(p1);
+//		
+//		for(Ville ville: villes) {
+//			Phrase p = new Phrase("Nom ville: "+ville.getNom()+", nombre d'habitants:"+ville.getNbHabitant() , new Font(baseFont, 32.0f, 1, new BaseColor(0, 51, 80)));
+//			document.add(p);
+//		}
+//		document.close();
+//		response.flushBuffer();
+//	}
 }
