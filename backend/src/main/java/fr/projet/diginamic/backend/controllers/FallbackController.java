f3fd7eba5cb52b5c4da2b6ec82a58105cd68347d
lysiandon <don.lysiane@gmail.com>
2024-07-27 19:22:29 +0200

    created fallback controller for 404 pages

---
+++ backend/src/main/java/fr/projet/diginamic/backend/controllers/FallbackController.java
@@ -0,0 +1,20 @@
+package fr.projet.diginamic.backend.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("/api")
+public class FallbackController {
+
+    @GetMapping("/**")
+    public ResponseEntity<String> handleAll() {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Page not found.");
+    }
+
+}
