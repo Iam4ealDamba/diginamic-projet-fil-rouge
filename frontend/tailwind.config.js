d821d6ca82859ee010d7f03b67421b62536268de
elkoura <elkourafouad@gmail.com>
2024-08-19 11:49:06 +0200

    config front

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/index.html",
+    "./src/**/*.{ts,html}"
+  ],
   theme: {
     extend: {},
   },
